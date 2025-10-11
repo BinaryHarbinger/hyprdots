eb8fd18987ef2d2a62c67b27036440244e9b6e3a
BinaryHarbinger <132145397+BinaryHarbinger@users.noreply.github.com>
2025-10-11 20:23:25 +0300

    Update qutebrowser theme

--- config/binarydots/themes/Gnome/theme.py
+++ config/binarydots/themes/Gnome/theme.py
@@ -1,13 +1,13 @@
 # Gruvbox Colors
 
-bg_main          = "#3C3836" 
-bg_hover         = "#928374"
-bg_active        = "#665c54"
-bg_alt           = "#504945"
+bg_main          = "#101010" 
+bg_hover         = "#383838"
+bg_active        = "#2D2D2D"
+bg_alt           = "#262626"
 bg_alt2          = "#7c6f64"
 
-content_main     = "#EBDBB2"
-content_hover    = "#EBDBB2"
+content_main     = "#F7F3F7"
+content_hover    = "#F7F3F7"
 content_alt      = "#665c54"
-content_act      = "#EBDBB2"
+content_act      = "#F7F3F7"
 content_inactive = "#928374"
