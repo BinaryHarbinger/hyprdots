ed0be850201f588fc8ecf1e2bc6fc2e117759097
BinaryHarbinger <halilefeesen@proton.me>
2025-12-29 01:00:09 +0300

    feat: Improve riftbar configuration

--- config/mako/scripts/riftbar.sh
+++ config/mako/scripts/riftbar.sh
@@ -1,42 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 
 logfile="$HOME/.local/share/mako/notifications.log"
 
 case "$1" in
-    --dnd-toggle|-d)
-        makoctl mode -t dnd 
-        exit 0
-        ;;
-    --status|-s)
-        ;;
-    "")
-        ;;
-    *)
-        echo "Unknown option: $1"
-        exit 1
-        ;;
+    --dnd-toggle|-d) makoctl mode -t dnd; exit 0 ;;
+    --status|-s|"") ;;
+    *) echo "Unknown option: $1"; exit 1 ;;
 esac
 
-if makoctl mode | grep -q '^dnd$'; then
-    dnd=1
-else
-    dnd=0
-fi
+dnd=0
+makoctl mode | grep -q '^dnd$' && dnd=1
 
-count=$(wc -l < "$logfile")
+new=0
+[ "$(wc -l < "$logfile")" -gt 0 ] && new=1
 
-if [[ $count -gt 0 ]]; then
-    new=1
-else
-    new=0
-fi
-
-if [[ $dnd -eq 1 && $new -eq 1 ]]; then
-    echo "<span foreground='gray' rise='7000' size='11000' letter_spacing='-24000'></span>"
-
-elif [[ $dnd -eq 1 && $new -eq 0 ]]; then
+if [ "$dnd" -eq 1 ] && [ "$new" -eq 1 ]; then
+    echo "<span rise='7000' size='11000' letter_spacing='-24000'></span>"
+elif [ "$dnd" -eq 1 ] && [ "$new" -eq 0 ]; then
     echo "<span foreground='gray' rise='7000' size='11000' letter_spacing='-24000'></span>"
-elif [[ $dnd -eq 0 && $new -eq 1 ]]; then
+elif [ "$dnd" -eq 0 ] && [ "$new" -eq 1 ]; then
     echo "󱅫"
 else
     echo "󰂚"
