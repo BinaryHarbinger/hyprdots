12497b0ec057ad7a3bc3940bec42b94b2fcba364
BinaryHarbinger <halilefeesen@proton.me>
2026-01-02 03:39:22 +0300

    feat: Improved widget handling

--- scripts/widgets.py
+++ scripts/widgets.py
@@ -2,6 +2,24 @@ from subprocess import run as system_command
 import json
 import sys
 import os
+import psutil
+from time import sleep
+
+def is_running(process_name: str) -> bool:
+    for proc in psutil.process_iter(attrs=["name"]):
+        if proc.info["name"] == process_name:
+            return True
+    return False
+
+
+def kill_process(name: str):
+    for proc in psutil.process_iter(attrs=["name"]):
+        if proc.info["name"] == name:
+            proc.terminate()   # SIGTERM
+            try:
+                proc.wait(timeout=2)
+            except psutil.TimeoutExpired:
+                proc.kill()    # SIGKILL
 
 arguments = sys.argv[1:]
 
@@ -18,6 +36,16 @@ data = default_data = {
 }
 
 default_data_json = json.dumps(default_data, indent=3)
+
+def reload_widgets():
+    if is_running("ewwii"):
+        system_command(["ewwii", "close-all"])
+        kill_process("ewwii")
+    while not is_running("ewwii"):
+        system_command(["ewwii", "d"])
+        sleep(0.2)
+    open_widgets(data)
+
 
 # Import the file
 def load_file():
@@ -41,7 +69,7 @@ def open_widgets(dataF=data):
             command = ["ewwii", "open", x]
             system_command(command)
 
-widget_arguments = {"one", "two", "three", "four"}
+widget_arguments = {"status", "deskclock", "desktopmusic", "activatelinux", "activate"}
 
 try:
     data = load_file()
@@ -49,35 +77,31 @@ except:
     pass
 
 for argument in arguments:
-    if argument in widget_arguments:
-        if argument == "one":
-            print("One detected")
+    if argument.lower() in widget_arguments:
+        argument = argument.lower()
+        if argument == "status":
+            print("Status detected")
             currentState = not data.get("status")
             data.update(status=int(currentState))
-        elif argument == "two":
-            print("Two detected")
+        elif argument == "desktopmusic":
+            print("Desktop Music detected")
             currentState = not data.get("desktopmusic")
             data.update(desktopmusic=int(currentState))
-        elif argument == "three":
-            print("Three detected")
+        elif argument == "deskclock":
+            print("Desktop Clock detected")
             currentState = not data.get("deskclock")
             data.update(deskclock=int(currentState))
-        elif argument == "four":
-            print("Four detected")
+        elif argument in ["activate", "activatelinux"]:
+            print("Activation detected")
+            argument = "activatelinux"
             currentState = not data.get("activatelinux")
             data.update(activatelinux=int(currentState))
-
+        system_command(["ewwii", "open", "--toggle", argument])
         print(data)
         write_file(data)
     else:
-        command = ["ewwii", "r"]
-        if argument == "s":
-            system_command(command)
-            open_widgets(data)
+        if argument in ["s", "init", "i"]:
+           reload_widgets() 
         elif argument in ["r", "reload"]:
-            system_command(["ewwii", "close-all"])
-            system_command(["pkill", "ewwii"])
-            system_command(["ewwii", "kill"])
-            system_command(["ewwii", "d"])
-            open_widgets(data)
+            reload_widgets() 
             print(data)
