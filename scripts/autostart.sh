25621c12ade17ef0e1ae44d87c3d350afb89dd46
BinaryHarbinger <halilefeesen@proton.me>
2026-03-29 20:29:33 +0300

    feat: Improve keybinds and seperate autostart commands

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,6 +1,8 @@
 # ==================== Start Up Commands ====================
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP QT_QPA_PLATFORMTHEME
 wlr-randr --output eDP-1 --off
+systemctl --user start xdg-desktop-portal &
+hypridle
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 (sleep 1 && python ~/Dotfiles/scripts/wallpapers.py --init) &
 (sleep 1.6 && python ~/Dotfiles/scripts/widgets.py s) &
